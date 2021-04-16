868a8d40d6928ce2f7a584b915dbbfc4fd3a1be9
Eryk Sikora <e.sikora95@gmail.com>
2021-04-16 20:59:28 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("./js/sw.js").then(
-      function (registration) {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      function (err) {
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
-auth.onAuthStateChanged((user) => {
-  const logout = document.getElementById("logout-btn");
-  const login = document.getElementById("login-btn");
-  if (user) {
-    console.log("user logged in: ", user.email);
-    //  login.style.display = 'none';
-    // logout.style.display = 'block';
-  } else {
-    console.log("user logged out");
-    //   logout.style.display = 'none';
-    //  login.style.display = 'block';
-  }
-});
-
 //logout
 const logout = document.getElementById("logout-btn");
 if (document.body.contains(logout)) {
